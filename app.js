83d7dc9aee30526b123adc75802cf5ab460c134b
sinelnikovv <aktep010@gmail.com>
2022-05-08 23:19:18 +0300

    add aos

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var splide = new Splide( '.splide', {
     type     : 'loop',
-    height   : '10rem',
+    height   : 'auto',
     focus    : 'center',
     autoWidth: true,
+    gap: "1.2rem",
+    pagination: false,
+    arrows: false,
   } );
   
   splide.mount();
+
+  AOS.init();
